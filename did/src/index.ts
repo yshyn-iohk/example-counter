3fee24cbb8f3dcb5fe8cd372da2bc6f839ef3d0a
Yurii Shynbuiev <yurii.shynbuiev@iohk.io>
2025-06-23 16:56:49 +0800

    feat: implement the DID contract
    Signed-off-by: Yurii Shynbuiev <yurii.shynbuiev@iohk.io>

--- did/src/index.ts
+++ did/src/index.ts
@@ -13,5 +13,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-export * as Counter from "./managed/did/contract/index.cjs";
+export * as MidnightDID from "./managed/did/contract/index.cjs";
 export * from "./witnesses";
--- did/src/witnesses.ts
+++ did/src/witnesses.ts
@@ -14,8 +14,5 @@
 // limitations under the License.
 
 // This is how we type an empty object.
-export type CounterPrivateState = {
-  privateCounter: number;
-};
 
 export const witnesses = {};
